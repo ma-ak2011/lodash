f4c66bed8ab19644feeccc27a9de13a76b309621
ma- <ponyonekoak@gmail.com>
2018-12-12 01:18:52 +0900

    change property name

--- lodashTest.js
+++ lodashTest.js
@@ -2,76 +2,76 @@ window.onload = function() {
     //芸能人好感度ランキングから名前を借りました。出身地や年齢は加工しています。
     const people =
     [
-        { Name : '明石家さんま', Age : 63, Sex : 'Male', BirthPlace : '東京都'},
-        { Name : '阿部寛', Age : 54, Sex : 'Male', BirthPlace : '神奈川県'},
-        { Name : 'マツコ・デラックス', Age : 46, Sex : 'Male', BirthPlace : '千葉県'},
-        { Name : '内村光良', Age : 54, Sex : 'Male', BirthPlace : '熊本県'},
-        { Name : '新垣結衣', Age : 30, Sex : 'Female', BirthPlace : '神奈川県'},
-        { Name : '浅田真央', Age : 28, Sex : 'Female', BirthPlace : '東京都'},
-        { Name : '綾瀬はるか', Age : 33, Sex : 'Female', BirthPlace : '熊本県'}
+        { Name : '明石家さんま', Age : 63, Gender : 'Male', BirthPlace : '東京都'},
+        { Name : '阿部寛', Age : 54, Gender : 'Male', BirthPlace : '神奈川県'},
+        { Name : 'マツコ・デラックス', Age : 46, Gender : 'Male', BirthPlace : '千葉県'},
+        { Name : '内村光良', Age : 54, Gender : 'Male', BirthPlace : '熊本県'},
+        { Name : '新垣結衣', Age : 30, Gender : 'Female', BirthPlace : '神奈川県'},
+        { Name : '浅田真央', Age : 28, Gender : 'Female', BirthPlace : '東京都'},
+        { Name : '綾瀬はるか', Age : 33, Gender : 'Female', BirthPlace : '熊本県'}
     ];
     
 
     const first = _.first(people);
     console.log(first);
-    //{Name: "明石家さんま", Age: 63, Sex: "Male", BirthPlace: "東京都"}
+    //{Name: "明石家さんま", Age: 63, Gender: "Male", BirthPlace: "東京都"}
 
-    const men = _.filter(people, p => p.Sex === 'Male');
+    const men = _.filter(people, p => p.Gender === 'Male');
     console.log(men);
-    //{Name: "明石家さんま", Age: 63, Sex: "Male", BirthPlace: "東京都"}
-    //{Name: "阿部寛", Age: 54, Sex: "Male", BirthPlace: "神奈川県"}
-    //{Name: "マツコ・デラックス", Age: 46, Sex: "Male", BirthPlace: "千葉県"}
-    //{Name: "内村光良", Age: 54, Sex: "Male", BirthPlace: "熊本県"}
+    //{Name: "明石家さんま", Age: 63, Gender: "Male", BirthPlace: "東京都"}
+    //{Name: "阿部寛", Age: 54, Gender: "Male", BirthPlace: "神奈川県"}
+    //{Name: "マツコ・デラックス", Age: 46, Gender: "Male", BirthPlace: "千葉県"}
+    //{Name: "内村光良", Age: 54, Gender: "Male", BirthPlace: "熊本県"}
 
-    const firstFemale = _.find(people, p => p.Sex === 'Female');
+    const firstFemale = _.find(people, p => p.Gender === 'Female');
     console.log(firstFemale);
-    //{Name: "新垣結衣", Age: 30, Sex: "Female", BirthPlace: "神奈川県"}
+    //{Name: "新垣結衣", Age: 30, Gender: "Female", BirthPlace: "神奈川県"}
 
     const existPersonFromKumamoto = _.some(people, p => p.BirthPlace === '熊本県');
     console.log(existPersonFromKumamoto);
     //true
 
-    //const areAllMale = _people.All(p => p.Sex == Sex.Male);
+    //const areAllMale = _people.All(p => p.Gender == Gender.Male);
 
     const orderedByAge = _.orderBy(people, ['Age'], ['asc']);
 
     console.log(orderedByAge);
-    //{Name: "浅田真央", Age: 28, Sex: "Female", BirthPlace: "東京都"}
-    //{Name: "新垣結衣", Age: 30, Sex: "Female", BirthPlace: "神奈川県"}
-    //{Name: "綾瀬はるか", Age: 33, Sex: "Female", BirthPlace: "熊本県"}
-    //{Name: "マツコ・デラックス", Age: 46, Sex: "Male", BirthPlace: "千葉県"}
-    //{Name: "阿部寛", Age: 54, Sex: "Male", BirthPlace: "神奈川県"}
-    //{Name: "内村光良", Age: 54, Sex: "Male", BirthPlace: "熊本県"}
-    //{Name: "明石家さんま", Age: 63, Sex: "Male", BirthPlace: "東京都"}
+    //{Name: "浅田真央", Age: 28, Gender: "Female", BirthPlace: "東京都"}
+    //{Name: "新垣結衣", Age: 30, Gender: "Female", BirthPlace: "神奈川県"}
+    //{Name: "綾瀬はるか", Age: 33, Gender: "Female", BirthPlace: "熊本県"}
+    //{Name: "マツコ・デラックス", Age: 46, Gender: "Male", BirthPlace: "千葉県"}
+    //{Name: "阿部寛", Age: 54, Gender: "Male", BirthPlace: "神奈川県"}
+    //{Name: "内村光良", Age: 54, Gender: "Male", BirthPlace: "熊本県"}
+    //{Name: "明石家さんま", Age: 63, Gender: "Male", BirthPlace: "東京都"}
 
     const orderedByAgeThenByName = _.orderBy(people, ['Age', 'Name'], ['desc', 'asc']);
 
     console.log(orderedByAgeThenByName);
-    //{Name: "明石家さんま", Age: 63, Sex: "Male", BirthPlace: "東京都"}
-    //{Name: "内村光良", Age: 54, Sex: "Male", BirthPlace: "熊本県"}
-    //{Name: "阿部寛", Age: 54, Sex: "Male", BirthPlace: "神奈川県"}
-    //{Name: "マツコ・デラックス", Age: 46, Sex: "Male", BirthPlace: "千葉県"}
-    //{Name: "綾瀬はるか", Age: 33, Sex: "Female", BirthPlace: "熊本県"}
-    //{Name: "新垣結衣", Age: 30, Sex: "Female", BirthPlace: "神奈川県"}
-    //{Name: "浅田真央", Age: 28, Sex: "Female", BirthPlace: "東京都"}
+    //{Name: "明石家さんま", Age: 63, Gender: "Male", BirthPlace: "東京都"}
+    //{Name: "内村光良", Age: 54, Gender: "Male", BirthPlace: "熊本県"}
+    //{Name: "阿部寛", Age: 54, Gender: "Male", BirthPlace: "神奈川県"}
+    //{Name: "マツコ・デラックス", Age: 46, Gender: "Male", BirthPlace: "千葉県"}
+    //{Name: "綾瀬はるか", Age: 33, Gender: "Female", BirthPlace: "熊本県"}
+    //{Name: "新垣結衣", Age: 30, Gender: "Female", BirthPlace: "神奈川県"}
+    //{Name: "浅田真央", Age: 28, Gender: "Female", BirthPlace: "東京都"}
 
     const groupByAndOrderedBy = _.groupBy(people, p => p.BirthPlace);
     console.log(groupByAndOrderedBy);
 
     //千葉県
-    //{Name: "マツコ・デラックス", Age: 46, Sex: "Male", BirthPlace: "千葉県"}
+    //{Name: "マツコ・デラックス", Age: 46, Gender: "Male", BirthPlace: "千葉県"}
 
     //東京都
-    //{Name: "明石家さんま", Age: 63, Sex: "Male", BirthPlace: "東京都"}
-    //{Name: "浅田真央", Age: 28, Sex: "Female", BirthPlace: "東京都"}
+    //{Name: "明石家さんま", Age: 63, Gender: "Male", BirthPlace: "東京都"}
+    //{Name: "浅田真央", Age: 28, Gender: "Female", BirthPlace: "東京都"}
 
     //熊本県
-    //{Name: "内村光良", Age: 54, Sex: "Male", BirthPlace: "熊本県"}
-    //{Name: "綾瀬はるか", Age: 33, Sex: "Female", BirthPlace: "熊本県"}
+    //{Name: "内村光良", Age: 54, Gender: "Male", BirthPlace: "熊本県"}
+    //{Name: "綾瀬はるか", Age: 33, Gender: "Female", BirthPlace: "熊本県"}
 
     //神奈川県
-    //{Name: "阿部寛", Age: 54, Sex: "Male", BirthPlace: "神奈川県"}
-    //{Name: "新垣結衣", Age: 30, Sex: "Female", BirthPlace: "神奈川県"}
+    //{Name: "阿部寛", Age: 54, Gender: "Male", BirthPlace: "神奈川県"}
+    //{Name: "新垣結衣", Age: 30, Gender: "Female", BirthPlace: "神奈川県"}
 
     const minBy = _.minBy(people, p => p.Age);
     console.log(minBy);
